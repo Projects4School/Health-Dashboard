c63a8f1090c037bdcaa4dee506c680653a05b6ff
Gaspar M <gaspar.musitelli@ecole-isitech.fr>
2024-02-07 15:59:11 +0100

    Add definitions file

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,16 @@
+export type Patient = {
+    id: string;
+    gender: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    image_url: string;
+};
+
+export type PatientField = {
+    gender: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    image_url: string;
+};
